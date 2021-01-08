37dfef96ebe923ba8528313254ad336d56705d0e
astalios <37106010+Astalios@users.noreply.github.com>
2021-01-08 11:51:25 +0100

    rewrite des erreurs : malloc, cd command, simplification du strtok TODO : Switch case, ecriture des bonus

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+// DOC : Mode canonique, ioctl, mode canonique / brut du terminal, gestion des signaux
+// management du terminal
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,82 +9,64 @@
 
 #define COMMAND_SIZE 64
 #define PATH_MAX 256
+#define TRUE 1
 
-int countCharacters(const char * str, const char character) {
-    int count = 0;
+int main(void) {
 
-    for (unsigned int i = 0; str[i]; i++) {
-        count += (str[i] == character);
-    }
+    // Get command
+    char *command = malloc(COMMAND_SIZE);
 
-    return count;
-}
+    char **arguments = malloc(COMMAND_SIZE * 4);
 
-int main(int argc, char **argv) {
-
-    while (1) {
-
-        // Get command
-        char * command = malloc(COMMAND_SIZE);
+    while (TRUE) {
 
         char cwd[PATH_MAX];
         getcwd(cwd, sizeof(cwd));
 
-        fputs(cwd, stdout);
-        fputs(" > ", stdout);
+        fprintf(stdout,"%s >", cwd);
         fgets(command, COMMAND_SIZE, stdin);
 
-        strtok(command, "\n"); // Remove \n from command
+        unsigned long len = strlen(command);
+        command[len -1] = '\0';
 
         // Space count + final argument + NULL
-        const int arguments_count = countCharacters(command, ' ') + 2;
-        char * arguments[arguments_count];
-
         // Get all arguments and place them in arguments array
-        char * token = strtok(command, " ");
+        char *token = strtok(command, " ");
         unsigned int i = 0;
         while (token != NULL) {
-            arguments[i++] = token;
+            *(arguments + i++) = token;
             token = strtok(NULL, " ");
         }
 
-        arguments[i] = NULL;
+        *(arguments + i)  = NULL;
 
+        // TODO: Faire un switch case
         if (strcmp(arguments[0], "exit") == 0) {
             exit(1);
-        }
+        } else if (strcmp(arguments[0], "cd") == 0) {
 
+            chdir(arguments[1]);
+            continue;
+        }
         // Fork
         pid_t id = fork();
-
 
         if (id > 0) {
             /* Parent process */
             wait(NULL);
         } else if (id == 0) {
             /* Child process */
-
-            if (strcmp(arguments[0], "cd") == 0) {
-
-                chdir(arguments[1]);
-
-            } else {
-
-                // Exec command
-                int err = execvp(arguments[0], arguments);
-
-                if (err == -1) {
-                    perror("Command failed");
-                    exit(EXIT_FAILURE);
-                }
-
+            // Exec command
+            int err = execvp(arguments[0], arguments);
+            if (err == -1) {
+                perror("Command failed");
+                exit(EXIT_FAILURE);
             }
         } else {
             /* Fork creation failed */
             printf("Fork creation failed!\n");
             break;
         }
-
     }
 
     return EXIT_SUCCESS;
